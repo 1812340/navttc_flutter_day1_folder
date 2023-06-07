f6b60672e89b570387b2b67f543bef2f228feb8e
waqas <cs1812340@szabist.pk>
2023-06-07 20:08:06 +0500

    navttc day one : folders

---
+++ lib/screens/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("kuch bi maju"),
+        leading: Icon(Icons.account_circle_outlined),
+        centerTitle:true ,
+      ),
+
+    body: Center(
+      child: Text("hello from home page"),
+    ),
+    );
+  }
+}
